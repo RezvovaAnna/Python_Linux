829ab4b516771c67ac9b5cb51b479f3a17954672
RezvovaAnna <119063467+RezvovaAnna@users.noreply.github.com>
2023-11-20 18:46:51 +0500

    Revert "Seminar 4"

--- test_negative.py
+++ test_negative.py
@@ -1,40 +1,29 @@
 import yaml
+from checkers import checkout_negative, getout, checkout
 
-from ssh_checkers import ssh_checkout_negative, ssh_checkout, ssh_get
 
 with open('config.yaml') as f:
+    # читаем документ YAML
     data = yaml.safe_load(f)
 
 
+
 class TestNegative:
-
-  
-    def save_log(self, starttime, name):
-        with open(name, 'w') as f:
-            f.write(ssh_get(data['host'], data['user'], data['passwd'], "journalctl --since '{}'".format(starttime)))
-
-            
-    def test_step1(self, clear_folders, make_files, make_bad_arx, start_time):
+    def test_step1(self, clear_folders, make_files, make_bad_arx) :
         # test1
         res = []
-        res.append(ssh_checkout_negative(data['host'], data['user'], data['passwd'],
-                                         "cd {}; 7z a {}/arxbad.{}".format(data['folder_in'], data['folder_out'],
-                                                                           data['type']), "ERROR:"))
-        res.append(ssh_checkout_negative(data['host'], data['user'], data['passwd'],
-                                         "cd {}; 7z e  arxbad.{} -o{} -y".format(data['folder_out'],
-                                                                                 data['folder_test'], data['type']), "ERROR:"))
-        self.save_log(start_time, "log_test_negative_1.txt")
+        res.append(checkout_negative("cd {}; 7z a {}/arxbad.{}".format(data['folder_in'], data['folder_out'], data['type']), "ERROR:"))
+        res.append(checkout_negative("cd {}; 7z e  arxbad.{} -o{} -y".format(data['folder_out'], data['folder_test'], data['type']), "ERROR:"))
         assert all(res), "test1 FAIL"
 
-        
-    def test_step2(self, clear_folders, make_files, start_time):
+
+    def test_step2(self, clear_folders, make_files):
         # test2
         res = []
-        res.append(ssh_checkout(data['host'], data['user'], data['passwd'],
-                                "cd {}; 7z a {}/arx2".format(data['folder_in'], data['folder_out']),
-                                "Everything is Ok"))
-        res.append(ssh_checkout_negative(data['host'], data['user'], data['passwd'],
-                                         "cd {}; 7z e arx3.{}".format(data['folder_out'], data['folder_test']), "ERROR:"))
-        self.save_log(start_time, "log_test_negative_2.txt")
+        res.append(checkout("cd {}; 7z a {}/arx2".format(data['folder_in'], data['folder_out']), "Everything is Ok"))
+        res.append(checkout_negative("cd {}; 7z e arx3.{}".format(data['folder_out'], data['folder_test']), "ERROR:"))
         assert all(res), "test2 FAIL"
+
+
+
 
--- test_positive.py
+++ test_positive.py
@@ -1,102 +1,78 @@
-from checkers import ssh_checkout, ssh_get
+from checkers import checkout, getout
 import yaml
 
 
 with open('config.yaml') as f:
+    # читаем документ YAML
     data = yaml.safe_load(f)
 
 
 class TestPositive:
-
-    def save_log(self, starttime, name):
-        with open(name, 'w') as f:
-            f.write(ssh_get(data['host'], data['user'], data['passwd'],"journalctl --since '{}'".format(starttime)))
-    def test_step1(self, clear_folders, make_files, start_time):
+    def test_step1(self, clear_folders, make_files):
         # test1
         res = []
-        res.append(ssh_checkout(data['host'], data['user'], data['passwd'],
-                                "cd {}; 7z a {}/arx2 -t{}".format(data['folder_in'], data['folder_out'], data['type']),
-                                "Everything is Ok"))
-        res.append(ssh_checkout(data['host'], data['user'], data['passwd'],
-                                "ls {}".format(data['folder_out']), "arx2.{}".format(data['type'])))
-        self.save_log(start_time, "log_test_positive_1.txt")
+        res.append(checkout("cd {}; 7z a {}/arx2 -t{}".format(data['folder_in'], data['folder_out'], data['type']),
+                            "Everything is Ok"))
+        res.append(checkout("ls {}".format(data['folder_out']), "arx2.{}".format(data['type'])))
         assert all(res), "test1 FAIL"
 
 
-    def test_step2(self, clear_folders, make_files, start_time):
+    def test_step2(self, clear_folders, make_files):
         # test2
         res = []
-        res.append(ssh_checkout(data['host'], data['user'], data['passwd'],
-                                "cd {}; 7z a {}/arx2".format(data['folder_in'], data['folder_out']),"Everything is Ok"))
-        res.append(ssh_checkout(data['host'], data['user'], data['passwd'],
-                                "cd {}; 7z e arx2.{} -o{} -y".format(data['folder_out'], data['type'],
-                                                                     data['folder_test']), "Everything is Ok"))
+        res.append(checkout("cd {}; 7z a {}/arx2".format(data['folder_in'], data['folder_out']), "Everything is Ok"))
+        res.append(checkout("cd {}; 7z e arx2.{} -o{} -y".format(data['folder_out'], data['type'], data['folder_test']), "Everything is Ok"))
         for item in make_files:
-            res.append(ssh_checkout(data['host'], data['user'], data['passwd'], "ls {}".format(data["folder_test"]), item))
-        self.save_log(start_time, "log_test_positive_2.txt")
+            res.append(checkout("ls {}".format(data["folder_test"]), item))
         assert all(res), "test2 FAIL"
 
 
-    def test_step3(self, start_time):
+    def test_step3(self):
         # test3
-        self.save_log(start_time, "log_test_positive_3.txt")
-        assert ssh_checkout(data['host'], data['user'], data['passwd'],
-                            "cd {}; 7z -t{} t arx2.{}".format(data['folder_out'],
-                                                              data['type'], data['type']), "Everything is Ok"), "test3 FAIL"
+        assert checkout("cd {}; 7z -t{} t arx2.{}".format(data['folder_out'],data['type'], data['type']),
+                        "Everything is Ok"), "test3 FAIL"
 
 
-    def test_step4(self, start_time):
+    def test_step4(self):
     # test4
-        self.save_log(start_time, "log_test_positive_4.txt")
-        assert ssh_checkout(data['host'], data['user'], data['passwd'],
-                            "cd {}; 7z -t{} u arx2.{}".format(data['folder_in'],
-                                                              data['type'], data['type']), "Everything is Ok"), "test4 FAIL"
+        assert checkout("cd {}; 7z -t{} u arx2.{}".format(data['folder_in'],data['type'], data['type']),
+                        "Everything is Ok"), "test4 FAIL"
 
 
-    def test_step5(self, start_time):
+    def test_step5(self):
     # test5
-        self.save_log(start_time, "log_test_positive_5.txt")
-        assert ssh_checkout(data['host'], data['user'], data['passwd'],
-                            "cd {}; 7z -t{} d arx2.{}".format(data['folder_out'],
-                                                              data['type'], data['type']), "Everything is Ok"), "test5 FAIL"
+        assert checkout("cd {}; 7z -t{} d arx2.{}".format(data['folder_out'],data['type'], data['type']),
+                        "Everything is Ok"), "test5 FAIL"
 
-    def test_step6(self, clear_folders, make_files, start_time):
+    def test_step6(self, clear_folders, make_files):
         # test6 homework seminar_2
         res = []
-        res.append(ssh_checkout(data['host'], data['user'], data['passwd'],
-                                "cd {}; 7z a -t{} {}/arx2.{}" .format(data['folder_in'],
-                                                                      data['type'], data['folder_out'], data['type']), "Everything is Ok"))
+        res.append(checkout("cd {}; 7z a -t{} {}/arx2.{}" .format(data['folder_in'], data['type'], data['folder_out'], data['type']),
+                            "Everything is Ok"))
         for item in make_files:
-            res.append(ssh_checkout(data['host'], data['user'], data['passwd'],
-                                    "cd {}; 7z l -t{} arx2.{}".format(data['folder_out'],
-                                                                      data['type'], data['type']), item))
-        self.save_log(start_time, "log_test_positive_6.txt")
+            res.append(checkout("cd {}; 7z l -t{} arx2.{}".format(data['folder_out'],data['type'], data['type']), item))
         assert all(res), "test6 FAIL"
 
-    def test_step7(self, clear_folders, make_files, make_subfolder, start_time):
+    def test_step7(self, clear_folders, make_files, make_subfolder):
         # test7 homework seminar_2
         res = []
-        res.append(ssh_checkout(data['host'], data['user'], data['passwd'],
-                                "cd {}; 7z a {}/arx2".format(data['folder_in'],
-                                                             data['folder_out']), "Everything is Ok"))
-        res.append(ssh_checkout(data['host'], data['user'], data['passwd'],
-                                "cd {}; 7z x arx2.{} -o{} -y".format(data['folder_out'],
-                                                                     data['type'], data['folder_test2']), "Everything is Ok"))
+        res.append(checkout("cd {}; 7z a {}/arx2".format(data['folder_in'], data['folder_out']),
+                            "Everything is Ok"))
+        res.append(checkout("cd {}; 7z x arx2.{} -o{} -y".format(data['folder_out'], data['type'], data['folder_test2']),
+                            "Everything is Ok"))
         for item in make_files:
-            res.append(ssh_checkout(data['host'], data['user'], data['passwd'], "ls {}".format(data["folder_test2"]), item))
-        res.append(ssh_checkout(data['host'], data['user'], data['passwd'], "ls {}".format(data["folder_test2"]), make_subfolder[0]))
-        self.save_log(start_time, "log_test_positive_7.txt")
+            res.append(checkout("ls {}".format(data["folder_test2"]), item))
+        res.append(checkout("ls {}".format(data["folder_test2"]), make_subfolder[0]))
         assert all(res), "test7 FAIL"
 
-    def test_step8(self, clear_folders, make_files, start_time):
+    def test_step8(self, clear_folders, make_files):
         #test8
         res = []
         for item in make_files:
-            res.append(ssh_checkout(data['host'], data['user'], data['passwd'], "cd {}; 7z h {}".format(data['folder_in'], item),
-                                    "Everything is Ok"))
-            hash = ssh_get(data['host'], data['user'], data['passwd'],"cd {}; crc32 {}".format(data['folder_in'], item)).upper()
-            res.append(ssh_checkout(data['host'], data['user'], data['passwd'],
-                                    "cd {}; 7z h {}".format(data['folder_in'], item), hash))
-        self.save_log(start_time, "log_test_positive_8.txt")
+            res.append(checkout("cd {}; 7z h {}".format(data['folder_in'], item), "Everything is Ok"))
+            hash = getout("cd {}; crc32 {}".format(data['folder_in'], item)).upper()
+            res.append(checkout("cd {}; 7z h {}".format(data['folder_in'], item), hash))
         assert all(res), "test8 FAIL"
+
+
 
